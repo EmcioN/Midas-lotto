d1e48c3c6f3acf0eb2ce7670c67a7929e80f8945
EmcioN <villly1990@gmail.com>
2026-03-31 21:49:29 +0100

    Add profile model for user subscription details

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=120, blank=True)
+    subscription_expiry = models.DateField(null=True, blank=True)
+    department = models.CharField(max_length=120, blank=True)
+
+    def __str__(self):
+        return self.user.username
