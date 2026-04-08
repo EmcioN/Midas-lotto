851525240869a820cef80846053bf19f74639396
EmcioN <villly1990@gmail.com>
2026-04-08 16:40:41 +0100

    Add lotto models for comments

--- lotto/models.py
+++ lotto/models.py
@@ -1,5 +1,4 @@
 from decimal import Decimal
-
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
@@ -44,3 +43,15 @@ class DrawImage(models.Model):
 
     def __str__(self):
         return f"Image for {self.draw.title}"
+
+class DrawComment(models.Model):
+    draw = models.ForeignKey(Draw, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"Comment by {self.user.username} on {self.draw.title}"    
