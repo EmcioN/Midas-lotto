14f0348dd58a9311cdab804d3ea9e8ca26441267
EmcioN <villly1990@gmail.com>
2026-04-08 23:17:49 +0100

    Add draw list and draw detail pages

--- lotto/views.py
+++ lotto/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from .models import Draw
 
-# Create your views here.
+
+def draw_list(request):
+    draws = Draw.objects.select_related('monthly_summary').all()
+    return render(request, 'lotto/draw_list.html', {'draws': draws})
+
+
+def draw_detail(request, draw_id):
+    draw = get_object_or_404(Draw, id=draw_id)
+    return render(request, 'lotto/draw_detail.html', {'draw': draw})
