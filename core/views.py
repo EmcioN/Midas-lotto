a4540b4594046e2d8ff4deb373a2ea099c7fee80
EmcioN <villly1990@gmail.com>
2026-04-08 21:50:59 +0100

    Display winnings and current draw on homepage, Create superuser

--- core/views.py
+++ core/views.py
@@ -1,4 +1,19 @@
+from django.db.models import Sum
 from django.shortcuts import render
 
+from lotto.models import MonthlySummary, Draw
+
+
 def home(request):
-    return render(request, 'core/home.html')
+    latest_month = MonthlySummary.objects.first()
+    current_draw = Draw.objects.filter(is_current=True).first()
+    overall_winnings = MonthlySummary.objects.aggregate(
+        total=Sum('total_winnings')
+    )['total'] or 0
+
+    context = {
+        'latest_month': latest_month,
+        'current_draw': current_draw,
+        'overall_winnings': overall_winnings,
+    }
+    return render(request, 'core/home.html', context)
