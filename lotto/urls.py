467e8018a032d97f7a550687bca401ef3a8fb267
EmcioN <villly1990@gmail.com>
2026-04-11 15:40:22 +0100

    Add URL route for joining subscriptions

--- lotto/urls.py
+++ lotto/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import draw_list, draw_detail
+from .views import draw_list, draw_detail, join_subscription
 
 urlpatterns = [
     path('draws/', draw_list, name='draw_list'),
     path('draws/<int:draw_id>/', draw_detail, name='draw_detail'),
+    path('subscriptions/join/', join_subscription, name='join_subscription'),
 ]
