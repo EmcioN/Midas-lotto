17744b399bc4d4387d0833d8ecbb8af149d45602
EmcioN <villly1990@gmail.com>
2026-04-21 20:12:21 +0100

    Add basic tests for homepage response and template

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class HomePageTests(TestCase):
+    def test_homepage_returns_200(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_homepage_uses_correct_template(self):
+        response = self.client.get(reverse('home'))
+        self.assertTemplateUsed(response, 'core/home.html')
